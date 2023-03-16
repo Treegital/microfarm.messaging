06bb08c15d3de89bcfd30523ce0723f7ea143c9a
Souheil Chelfouh <trollfot@gmail.com>
2023-03-16 14:58:17 +0100

    Exposing messages class

---
+++ src/microfarm/messaging/__init__.py
@@ -0,0 +1,17 @@
+from microfarm.messaging.messages.requests_pb2 import (
+    RegistrationRequest,
+    TokenRequest,
+    RegistrationValidation,
+    CertificateCreationRequest,
+    CertificateRevocationRequest
+)
+
+from microfarm.messaging.messages.persistence_pb2 import (
+    CreateAccount,
+    CreateCertificate,
+    RevokeCertificate
+)
+
+from microfarm.messaging.messages.notification_pb2 import (
+    EmailNotification
+)
